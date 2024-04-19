946f5662ca777a3f0877eb4b8cdc8b6cb5228b69
elijahr <elijahr@users.noreply.github.com>
2024-04-19 00:45:00 -0500

    Better tagging/caching for docker build

--- scripts/build-and-push-docker-images.bash
+++ scripts/build-and-push-docker-images.bash
@@ -13,10 +13,10 @@ set -uexo pipefail
 
 case $(uname -m) in
   x86_64 | amd64)
-    platform=linux/amd64
+    current_platform="linux/amd64"
     ;;
   arm64 | aarch64)
-    platform=linux/arm64
+    current_platform="linux/arm64"
     ;;
   *)
     echo >&2
@@ -25,12 +25,44 @@ case $(uname -m) in
     ;;
 esac
 
+declare -a docker_tags=()
+
+# regex to parse semver 2.0.0, with pre-release and build number
+git_branch_or_tag_name=${GITHUB_REF_NAME:-$(git rev-parse --abbrev-ref HEAD)}
+
+if [[ -z $git_branch_or_tag_name ]]; then
+  git_branch_or_tag_name=$(git rev-parse --abbrev-ref HEAD)
+fi
+
+push_platforms="$current_platform"
+if [[ ${git_branch_or_tag_name:-} =~ ^v?(([0-9]+)\.([0-9]+)\.([0-9]+)(-([a-zA-Z0-9-]+))?(\+([a-zA-Z0-9\.-]+))?)$ ]]; then
+  major="${BASH_REMATCH[2]}"
+  minor="${BASH_REMATCH[3]}"
+  patch="${BASH_REMATCH[4]}"
+
+  if [ -z "${BASH_REMATCH[5]}" ]; then
+    # not a pre-release, tag latest
+    main_tag="latest"
+    docker_tags+=("latest" "v${major}" "v${major}.${minor}" "v${major}.${minor}.${patch}")
+  else
+    # pre-release, just use the tag
+    main_tag="${git_branch_or_tag_name//[^a-zA-Z0-9]/-}"
+    docker_tags+=("$main_tag")
+  fi
+  # tag so push to all platforms
+  push_platforms="linux/amd64,linux/arm64"
+else
+  # not a semantic version, just use the branch or tag
+  main_tag="${git_branch_or_tag_name//[^a-zA-Z0-9]/-}"
+  docker_tags+=("$main_tag")
+fi
+
 # build for the current platform
 docker buildx build \
-  --platform=$platform \
+  --platform="$current_platform" \
   --progress=plain \
   --cache-from=elijahru/lintball \
-  --tag=elijahru/lintball:latest \
+  --tag="elijahru/lintball:${main_tag}" \
   --target=lintball-latest \
   --load \
   .
@@ -40,70 +72,52 @@ export LINTBALL_WORKSPACE="${GITHUB_WORKSPACE:-${PWD}}"
 
 # Sanity check
 docker run \
-  --volume "${LINTBALL_WORKSPACE:-.}:/workspace:cached" \
-  --volume ./bin:/lintball/bin:cached \
-  --volume ./configs:/lintball/configs:cached \
-  --volume ./lib:/lintball/lib:cached \
-  --volume ./scripts:/lintball/scripts:cached \
-  --volume ./test:/lintball/test:cached \
-  elijahru/lintball:latest \
+  --platform="$current_platform" \
+  --volume="${LINTBALL_WORKSPACE:-.}:/workspace:cached" \
+  --volume=./bin:/lintball/bin:cached \
+  --volume=./configs:/lintball/configs:cached \
+  --volume=./lib:/lintball/lib:cached \
+  --volume=./scripts:/lintball/scripts:cached \
+  --volume=./test:/lintball/test:cached \
+  "elijahru/lintball:${main_tag}" \
   lintball check
 
-# Build the test image
-docker buildx build \
-  --platform=$platform \
-  --progress=plain \
-  --cache-from=elijahru/lintball \
-  --tag=elijahru/lintball:test \
-  --target=lintball-test \
-  --load \
-  .
+# # Build the test image
+# docker buildx build \
+#   --platform="$platforms_csv" \
+#   --progress=plain \
+#   --cache-from=elijahru/lintball \
+#   --tag=elijahru/lintball:test \
+#   --target=lintball-test \
+#   --load \
+#   .
 
 # # Run the tests
-# docker run \
-#   --volume "${LINTBALL_WORKSPACE:-.}:/workspace:cached" \
-#   --volume ./bin:/lintball/bin:cached \
-#   --volume ./configs:/lintball/configs:cached \
-#   --volume ./lib:/lintball/lib:cached \
-#   --volume ./scripts:/lintball/scripts:cached \
-#   --volume ./test:/lintball/test:cached \
-#   elijahru/lintball:test \
-#   npm run test
-
-declare -a tags=()
-
-# regex to parse semver 2.0.0, with pre-release and build number
-if [[ ${GITHUB_REF_NAME:-} =~ ^v?(([0-9]+)\.([0-9]+)\.([0-9]+)(-([a-zA-Z0-9-]+))?(\+([a-zA-Z0-9\.-]+))?)$ ]]; then
-  major="${BASH_REMATCH[2]}"
-  minor="${BASH_REMATCH[3]}"
-  patch="${BASH_REMATCH[4]}"
-
-  if [ -z "${BASH_REMATCH[5]}" ]; then
-    # not a pre-release, tag latest
-    tags+=("latest" "v${major}" "v${major}.${minor}" "v${major}.${minor}.${patch}")
-  else
-    # pre-release, just use the tag
-    tags+=("${GITHUB_REF_NAME//[^a-zA-Z0-9]/-}")
-  fi
-  push_platforms="linux/amd64,linux/arm64"
-else
-  # not a semantic version, just use the branch or tag
-  tags+=("${GITHUB_REF_NAME//[^a-zA-Z0-9]/-}")
-  # only push for the current platform; speeds up future builds on this branch
-  push_platforms="${platform}"
-fi
-
-echo "Pushing tags ${tags@Q}"
-echo "Pushing platforms ${push_platforms@Q}"
-
-# for tag in "${tags[@]}"; do
-#   # build for the current platform
-#   docker buildx build \
-#     --platform="${push_platforms}" \
-#     --progress=plain \
-#     --cache-from=elijahru/lintball \
-#     --tag="elijahru/lintball:${tag}" \
-#     --target=lintball-latest \
-#     --push \
-#     .
+# for platform in "${platforms[@]}"; do
+#   docker run \
+#     --platform "$platform" \
+#     --volume "${LINTBALL_WORKSPACE:-.}:/workspace:cached" \
+#     --volume ./bin:/lintball/bin:cached \
+#     --volume ./configs:/lintball/configs:cached \
+#     --volume ./lib:/lintball/lib:cached \
+#     --volume ./scripts:/lintball/scripts:cached \
+#     --volume ./test:/lintball/test:cached \
+#     elijahru/lintball:test \
+#     npm run test
 # done
+
+echo "Pushing tags: ${docker_tags@Q}"
+echo "Pushing platforms: ${push_platforms}"
+
+for tag in "${docker_tags[@]}"; do
+  # build for the current platform
+  docker buildx build \
+    --platform="$push_platforms" \
+    --progress=plain \
+    --cache-from="elijahru/lintball:${main_tag}" \
+    --cache-from=elijahru/lintball \
+    --tag="elijahru/lintball:${tag}" \
+    --target=lintball-latest \
+    --push \
+    .
+done
