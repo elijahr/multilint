726edf0957d0bc5a020ca6254d2ffdf8ccfbd3c3
elijahr <elijahr@users.noreply.github.com>
2024-11-04 15:08:48 -0600

    Fetch depth 0

--- scripts/action.sh
+++ scripts/action.sh
@@ -74,14 +74,14 @@ else
       # A pull request.
       # Check files which have changed between the merge base and the
       # current commit.
-      git fetch origin "${GITHUB_BASE_REF}"
+      git fetch --depth=0 origin "${GITHUB_BASE_REF}"
       git show-ref
       IFS= read -r committish < <(git merge-base -a "refs/remotes/origin/${GITHUB_BASE_REF}" "${GITHUB_SHA}" || true)
     else
       # A push to a non-default, non-PR branch.
       # Check files which have changed between default branch and the current
       # commit.
-      git fetch origin "${default_branch}"
+      git fetch  --depth=0 origin "${default_branch}"
       git show-ref
       IFS= read -r committish < <(git merge-base -a "refs/remotes/origin/${default_branch}" "${GITHUB_SHA}" || true)
     fi
